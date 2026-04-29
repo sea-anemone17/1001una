fffee34e417d46533e707387975aa7a59b0d4f8c
sea-anemone17 <leehaeju09@gmail.com>
2026-04-29 12:51:53 +0900

    Add sample chapter data to chapters.js

---
+++ data/chapters.js
@@ -0,0 +1,9 @@
+export const CHAPTERS = [
+  {
+    id: "CH01",
+    title: "Chapter 1 샘플",
+    description: "1001una v2 구조 검증용 샘플 챕터입니다.",
+    sentenceIds: [1, 2, 3],
+    status: "sample"
+  }
+];
