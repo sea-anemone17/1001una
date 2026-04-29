0f001cf908e09fbd6455c40f192806d78a2816af
sea-anemone17 <leehaeju09@gmail.com>
2026-04-29 12:51:26 +0900

    Add utility functions for DOM manipulation

---
+++ utils/dom.js
@@ -0,0 +1,11 @@
+export function $(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+export function $all(selector, root = document) {
+  return [...root.querySelectorAll(selector)];
+}
+
+export function setHTML(container, html) {
+  container.innerHTML = html;
+}
